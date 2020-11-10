1e552a878519fa53474a7f34c5f0e04c42e25e8c
Max Schäufele <max.schaeufele@gmail.com>
2020-11-10 09:44:57 +0100

    Data extraction | preprocessing | visualisation done

--- CSV_Path_Functions.py
+++ CSV_Path_Functions.py
@@ -4,6 +4,44 @@ import numpy as np
 import re
 
 
+def create_point(str_point):
+    str_point = str_point.replace("(", "").replace(")", "")
+
+    coordinates = list(map(int, str_point.split(",")))
+
+    point = np.array(coordinates)
+
+    return point
+
+
+def create_path(str_path):
+    str_path = str_path.replace("{", "").replace("}", "")
+
+    str_points = re.findall("\([^\)]*\)", str_path)
+
+    points = np.array([np.array([])])
+
+    for str_point in str_points:
+        point = create_point(str_point)
+        print("Point " + str(point))
+        points[0] = np.append(points, point, axis=0)
+        print("Points " + str(points))
+        #points = np.vstack((points, point))
+
+    print(points)
+    return points
+
+
+def create_paths(path_array):
+    paths = np.array([])
+
+    for path in path_array:
+        tmp_path = create_path(path)
+        paths = np.append(paths, tmp_path)
+
+    return paths
+
+'''
 def create_paths(path_array):
     paths = np.array([])
 
@@ -38,3 +76,4 @@ def create_point(str_point):
     point = Point(int(str_coordinates[0]), int(str_coordinates[1]))
 
     return point
+'''
--- Tests/CSV_Path_Functions_Tests.py
+++ Tests/CSV_Path_Functions_Tests.py
@@ -8,6 +8,35 @@ class MyTestCase(unittest.TestCase):
         str_point = "(123,456)"
         point = pf.create_point(str_point)
 
+        self.assertEqual(point[0], 123, "X isn't correct")
+        self.assertEqual(point[1], 456, "Y isn't correct")
+
+    def test_create_path(self):
+        str_path = "{\"(107096,18237)\",\"(106127,19080)\",\"(105253,20554)\",\"(105136,23198)\",\"(103262,24448)\"}"
+        path = pf.create_path(str_path)
+
+        print(type(path))
+        print(path)
+
+        self.assertEqual(path[0][0][0], 107096, "X isn't correct")
+        self.assertEqual(path[0][0][1], 18237, "Y isn't correct")
+
+    def test_create_paths(self):
+        paths_array = np.array(
+            ["{\"(107096,18237)\",\"(16127,19080)\",\"(105253,2054)\",\"(10136,2318)\",\"(10362,2448)\"}",
+             "{\"(207096,1837)\",\"(10617,1980)\",\"(10253,20554)\",\"(10516,23198)\",\"(103262,2448)\"}",
+             "{\"(307096,1237)\",\"(10612,1908)\",\"(15253,2554)\",\"(10513,2398)\",\"(10262,2448)\"}"])
+
+        paths = pf.create_paths(paths_array)
+
+        self.assertEqual(paths[0].points[0].x, 107096, "X isn't correct")
+        self.assertEqual(paths[0].points[0].y, 18237, "Y isn't correct")
+
+'''
+    def test_create_point(self):
+        str_point = "(123,456)"
+        point = pf.create_point(str_point)
+
         self.assertEqual(point.x, 123, "X isn't correct")
         self.assertEqual(point.y, 456, "Y isn't correct")
 
@@ -27,6 +56,7 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(paths[0].points[0].x, 107096, "X isn't correct")
         self.assertEqual(paths[0].points[0].y, 18237, "Y isn't correct")
+'''
 
 if __name__ == '__main__':
     unittest.main()
